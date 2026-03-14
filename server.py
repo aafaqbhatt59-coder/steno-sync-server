91dc0deff95b01720c19ff78fc8f9d132b686ef5
aafaqbhatt59-coder <aafaqbhatt59@gmail.com>
2026-03-14 12:41:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -117,5 +117,8 @@ def get_users():
     return jsonify(users)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
